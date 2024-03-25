51095f2be29ac5aeaee0383b6496c231495e7269
verss21 <arjunafrizal01@gmail.com>
2024-03-25 10:10:53 +0700

    Add project files.

---
+++ PertemuanKellima/PertemuanKellima.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+void input()
+{
+	cout << "Bilangan pertama = ";
+	cin >> a;
+
+	cout << "Bilangan kedua = ";
+	cin >> b;
+}
+
+int penjumlahan(int bil_1, int bil_2) {
+	return bil_1 + bil_2;
+}
+
+int pengurangan(int bil_1, int bil_2) {
+	return bil_1 - bil_2;
+}
+
+int perkalian(int bil_1, int bil_2) {
+	return bil_1 * bil_2;
+}
+
+int pembagian(int bil_1, int bil_2) {
+	return bil_1 / bil_2;
+}
+
+
