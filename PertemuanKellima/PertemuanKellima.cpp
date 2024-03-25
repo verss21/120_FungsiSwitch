0a401692186adfc83c7ec368e166dad239df9e86
verss21 <arjunafrizal01@gmail.com>
2024-03-25 10:23:42 +0700

    membuat command menu

--- PertemuanKellima/PertemuanKellima.cpp
+++ PertemuanKellima/PertemuanKellima.cpp
@@ -31,5 +31,15 @@ int main()
 {
 	int pilihan;
 
+	do {
+		system("cls");
+		cout << "======== Menu =========" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Masukan menu yang ingin di pilih (1/2/3/4/5): ";
+		cin >> pilihan;
 
 
